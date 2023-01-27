92656ebc25378c0f290f2fa069829b18689b0925
Ma. Darlene A. Arellano <119909626+arlln-yn@users.noreply.github.com>
2023-01-27 10:57:36 +0800

    Add files via upload

---
+++ Exercise/Exercise #4/App.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args) {
+
+        Person Friend = new Person("Kylie", 19);
+        
+        Myself Me = new Myself("Yeyen", 19);
+
+        // Aggregation
+        Me.addFriend(Friend);
+        
+        // Composition 
+        Pet dog = new Pet ("Sanjie", Friend);
+        dog.showOwnerName();
+
+        Car pickUp = new Car("Ford Ranger", "white", Me);
+        pickUp.showOwnerName();
+    }
+    
+}
